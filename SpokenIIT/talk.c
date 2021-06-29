baf3f8ceeffe9a07e551a9cd177dc927dd075c0d
Shyed Shahriar <48835958+shyed2001@users.noreply.github.com>
2021-06-30 02:42:45 +0600

    ok
    ok

---
+++ SpokenIIT/talk.c
@@ -0,0 +1,14 @@
+// My First C Program
+
+#include <stdio.h>
+
+int main(void)
+{
+   printf("Talk To a Teacher, welcome to the world of C  \n");
+   puts("Welcome "); 
+   puts("Welcome \n");
+
+   puts("Welcome ");
+   
+   return 0;
+}
